0e16034e432a48c42ea6520e3e1eb0a7433e29d3
alvaro.aranda <alvaro.aranda@deiser.com>
2021-02-11 02:14:06 +0100

    Add base skeleton to deploy a Maven App using Spring Boot

---
+++ src/main/java/com/varitoooo/hexagonal/beans/BeanConfig.java
@@ -0,0 +1,12 @@
+package com.varitoooo.hexagonal.beans;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+
+})
+public class BeanConfig {
+
+}
