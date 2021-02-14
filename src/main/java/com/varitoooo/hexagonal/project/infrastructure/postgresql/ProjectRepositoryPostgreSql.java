8bc88e78ef1c13b87b83be9ca22c40be2de6444c
alvaroaranda05 <alvaro.aranda05@gmail.com>
2021-02-14 22:08:19 +0100

    Unused import refactor

--- src/main/java/com/varitoooo/hexagonal/project/infrastructure/postgresql/ProjectRepositoryPostgreSql.java
+++ src/main/java/com/varitoooo/hexagonal/project/infrastructure/postgresql/ProjectRepositoryPostgreSql.java
@@ -3,9 +3,7 @@ package com.varitoooo.hexagonal.project.infrastructure.postgresql;
 import com.varitoooo.hexagonal.project.domain.Project;
 import com.varitoooo.hexagonal.project.domain.ProjectId;
 import com.varitoooo.hexagonal.project.domain.ProjectRepository;
-import org.hibernate.Criteria;
 
-import java.util.List;
 import java.util.Optional;
 
 public final class ProjectRepositoryPostgreSql implements ProjectRepository {
